a661a461545b7cb200fb688db12fe8cac773046c
2404574k <2404574k@student.gla.ac.uk>
2024-02-10 22:21:52 +0000

    added western conf table and restyled tables

--- routes.py
+++ routes.py
@@ -18,27 +18,37 @@ def index():
 
     json = newresponse.json()
     response_list = json['response']
-    teams = []
+    eastteams = []
+    westteams = []
     for team in response_list:
-        if team['conference']['name'] == 'east':
-            dic = {}
-            dic['name'] = team['team']['name']
-            dic['rank'] = team['conference']['rank']
-            dic['win'] = team['conference']['win']
-            dic['loss'] = team['conference']['loss']
-            dic['percentage'] = team['win']['percentage']
-            if  team['gamesBehind'] == None:
-                dic['gb'] = 0
-            else:
-                dic['gb'] = team['gamesBehind']
-            dic['strk'] = team['streak']
-            dic['last10'] = f"{team['win']['lastTen']}-{team['loss']['lastTen']}"
+        # if team['conference']['name'] == 'east':
+        dic = {}
+        dic['logo'] = team['team']['logo']
+        dic['name'] = team['team']['name']
+        dic['rank'] = team['conference']['rank']
+        dic['win'] = team['conference']['win']
+        dic['loss'] = team['conference']['loss']
+        dic['percentage'] = team['win']['percentage']
+        if  team['gamesBehind'] == None:
+            dic['gb'] = 0
+        else:
+            dic['gb'] = team['gamesBehind']
+        if team['winStreak'] == True:
+            dic['worlstreak'] = 'W'
+        elif team['winStreak'] == False:
+            dic['worlstreak'] = 'L'
+        dic['strk'] = team['streak']
+        dic['last10'] = f"{team['win']['lastTen']}-{team['loss']['lastTen']}"
             
-            teams.append(dic)
+        if team['conference']['name'] == 'east':
+            eastteams.append(dic)
+        elif team['conference']['name'] == 'west':
+            westteams.append(dic)        
         
-    ranked = sorted(teams, key=lambda x: x['rank'])
+    eastranked = sorted(eastteams, key=lambda x: x['rank'])
+    westranked = sorted(westteams, key=lambda x: x['rank'])
     
-    return render_template('index.html', teams=ranked)
+    return render_template('index.html', teams=eastranked, wteams=westranked)
  
  
 if __name__ == '__main__':
